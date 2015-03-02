b0f0055024f365b511649f881d029c7e834c0039
Andre D <andre@andred.ca>
2015-03-02 01:11:17 -0500

    Self post support

--- SporkUI.js
+++ SporkUI.js
@@ -11,22 +11,62 @@ Spork.UI.Item = Backbone.View.extend({
 
 Spork.UI.LinkItem = Backbone.View.extend({
 	className: 'item linkitem',
+	events: {
+		 'click .self .thumbnail a': 'showself',
+    },
+    renderself: function() {
+    	this.paragraph = $('<div>').css('height', 0).addClass('hider').append($('<p>').addClass('selftext'))
+    	this.paragraph.find('.selftext').html($("<div>").html(this.model.get('selftext_html')).text())
+    	this.$el.append(this.paragraph)
+    },
+    toggle: function() {
+    	if (this.paragraph.hasClass('shown')) {
+    		this.paragraph.animate({height: 0}, 250, 'linear');
+    	} else {
+    		var fullheight = this.paragraph.find('.selftext').height();
+    		this.paragraph.animate({height: fullheight}, 250, 'linear');
+    	}
+    	this.paragraph.toggleClass('shown')
+    },
+    showself: function(e) {
+    	e ? e.preventDefault() : undefined
+    	if (!this.paragraph) {
+    		this.renderself()
+    	}
+    	window.setTimeout(this.toggle.bind(this), 0)
+    },
 	thumbnail: function() {
 		var thumbnail = this.model.get('thumbnail')
 		
-		if (thumbnail == 'self') {
+		if (thumbnail == 'self' || this.model.get('is_self')) {
 			return 'https://www.reddit.com/static/self_default2.png'
 		}
 		
 		return thumbnail ? thumbnail : 'https://www.reddit.com/static/noimage.png'
 	},
+	link: function() {
+		if (this.model.get('is_self')) {
+			return '#'
+		}
+		return this.model.get('url')
+	},
 	permalink: function() {
 		return 'https://i.reddit.com' + this.model.get('permalink')
 	},
 	render: function() {
-		this.$el.html('').append(
+		if (this.model.get('is_self')) {
+			this.$el.addClass('selfitem')
+		}
+		
+		var contents = $('<div>').addClass('content')
+		
+		if (this.model.get('is_self')) {
+			contents.addClass('self')
+		}
+		
+		contents.append(
 				$('<div>').addClass('thumbnail').append(
-					$('<a>').attr('target', '_blank').attr('href', this.model.get('url')).append(
+					$('<a>').attr('target', '_blank').attr('href', this.link()).append(
 							$('<img>').attr('width', 128).attr('src', this.thumbnail())
 					)
 				)
@@ -34,8 +74,13 @@ Spork.UI.LinkItem = Backbone.View.extend({
 				$('<div>').addClass('title').append(
 						
 						$('<a>').attr('href',this.permalink()).text(this.model.get('title'))
+			
+				
 				)
 		)
+		
+		this.$el.html('').append(contents)
+		
 		return this
 	}
 })
